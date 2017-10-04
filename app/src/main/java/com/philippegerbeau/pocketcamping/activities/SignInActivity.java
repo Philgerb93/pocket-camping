d38c6e97393a260c610b0b14ffc720072c0c305b
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2017-10-04 01:56:48 -0400

    Add SignIn and SignUp Activities

---
+++ app/src/main/java/com/philippegerbeau/pocketcamping/activities/SignInActivity.java
@@ -0,0 +1,18 @@
+package com.philippegerbeau.pocketcamping.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.philippegerbeau.pocketcamping.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+    }
+}
