f5906ca3229cf5cd9a1e537e150dbf50f5c581ae
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2017-10-08 14:13:58 -0400

    Modify HomeActivity's layout

--- app/src/main/java/com/philippegerbeau/pocketcamping/activities/HomeActivity.java
+++ app/src/main/java/com/philippegerbeau/pocketcamping/activities/HomeActivity.java
@@ -1,11 +1,11 @@
 package com.philippegerbeau.pocketcamping.activities;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.philippegerbeau.pocketcamping.R;
 import com.philippegerbeau.pocketcamping.fragments.AlertsFragment;
@@ -13,43 +13,68 @@ import com.philippegerbeau.pocketcamping.fragments.ProfileFragment;
 import com.philippegerbeau.pocketcamping.fragments.StayFragment;
 
 public class HomeActivity extends AppCompatActivity {
-    BottomNavigationView navigation;
-
     Fragment stayFragment;
     Fragment alertsFragment;
     Fragment profileFragment;
+
+    ImageButton homeButton;
+    ImageButton alertsButton;
+    ImageButton profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        navigation = (BottomNavigationView) (findViewById(R.id.navigation));
-
         stayFragment = new StayFragment();
         alertsFragment = new AlertsFragment();
         profileFragment = new ProfileFragment();
 
-        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.action_home:
-                        getSupportFragmentManager().beginTransaction().replace(
-                                R.id.fragment_container, stayFragment).commit();
-                        break;
-                    case R.id.action_alerts:
-                        getSupportFragmentManager().beginTransaction().replace(
-                                R.id.fragment_container, alertsFragment).commit();
-                        break;
-                    case R.id.action_profile:
-                        getSupportFragmentManager().beginTransaction().replace(
-                                R.id.fragment_container, profileFragment).commit();
-                        break;
-                }
-                return true;
-            }
-        });
-        navigation.setSelectedItemId(R.id.action_home);
+        homeButton = (ImageButton) findViewById(R.id.action_home);
+        alertsButton = (ImageButton) findViewById(R.id.action_alerts);
+        profileButton = (ImageButton) findViewById(R.id.action_profile);
+
+        getSupportFragmentManager().beginTransaction().replace(
+                R.id.fragment_container, stayFragment).commit();
+    }
+
+    public void navigate(View view) {
+        switch (view.getId()) {
+            case R.id.action_home:
+                toHome();
+                break;
+            case R.id.action_alerts:
+                toAlerts();
+                break;
+            case R.id.action_profile:
+                toProfile();
+                break;
+        }
+    }
+
+    private void toHome() {
+        getSupportFragmentManager().beginTransaction().replace(
+                R.id.fragment_container, stayFragment).commit();
+        homeButton.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary));
+        alertsButton.setColorFilter(ContextCompat.getColor(this, R.color.colorTextPrimary));
+        profileButton.setColorFilter(ContextCompat.getColor(this, R.color.colorTextPrimary));
+    }
+
+    private void toAlerts() {
+        getSupportFragmentManager().beginTransaction().replace(
+                R.id.fragment_container, alertsFragment).commit();
+
+        homeButton.setColorFilter(ContextCompat.getColor(this, R.color.colorTextPrimary));
+        alertsButton.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary));
+        profileButton.setColorFilter(ContextCompat.getColor(this, R.color.colorTextPrimary));
+    }
+
+    private void toProfile() {
+        getSupportFragmentManager().beginTransaction().replace(
+                R.id.fragment_container, profileFragment).commit();
+
+        homeButton.setColorFilter(ContextCompat.getColor(this, R.color.colorTextPrimary));
+        alertsButton.setColorFilter(ContextCompat.getColor(this, R.color.colorTextPrimary));
+        profileButton.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary));
     }
 }
